ea028d331a2846a8ba7698e6ef9d2e86020c83e7
unknown <ru57kon>
2019-11-21 19:23:02 +0100

    added seeds-file

--- bin/seeds2.js
+++ bin/seeds2.js
@@ -1,56 +1,38 @@
-// Seeds file that remove all users and create 2 new users
-
-// To execute this seed, run from the root of the project
-// $ node bin/seeds.js
-
 const mongoose = require("mongoose");
+
 const bcrypt = require("bcrypt");
-const User = require("../models/User");
-const Toilet = require("../models/Toilet")
+
+const Toilet = require("../models/Toilet");
+
+​
 
 const bcryptSalt = 10;
 
+​
+
 mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://localhost/project2-app', {
+
+  .connect("mongodb://localhost/project2-app", {
     useNewUrlParser: true
   })
+
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+
+    console.log(
+
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+
+    );
+
   })
+
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+
+    console.error("Error connecting to mongo", err);
+
   });
 
-let users = [{
-    username: "alice",
-    password: bcrypt.hashSync("alice", bcrypt.genSaltSync(bcryptSalt)),
-  },
-  {
-    username: "bob",
-    password: bcrypt.hashSync("bob", bcrypt.genSaltSync(bcryptSalt)),
-  }
-]
-
-User.deleteMany()
-  .then(() => {
-    return User.create(users)
-  })
-  .then(usersCreated => {
-    console.log(`${usersCreated.length} users created with the following id:`);
-    console.log(usersCreated.map(u => u._id));
-  })
-  .then(() => {
-    // Close properly the connection to Mongoose
-    mongoose.disconnect()
-  })
-  .catch(err => {
-    mongoose.disconnect()
-    throw err
-  })
-
-
-///////////////////////////////////////////////////////////
-
+​
 
 let toilets = [
 
@@ -289,6 +271,8 @@ let toilets = [
   }
 
 ];
+
+​
 
 Toilet.deleteMany()
 
